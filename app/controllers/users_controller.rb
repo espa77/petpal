f747005151edbad4fd32f387258809ab01f2dd97
Matteo  Ziff <matteoziff@Matteos-MacBook-Pro.local>
2016-01-29 15:47:55 -0500

    fixed urls, show page, home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user=set_user
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
   root to: 'home#front'
   get 'posts/:id/destroy', to: 'posts#destroy'
@@ -8,6 +7,8 @@ Rails.application.routes.draw do
     get 'login' => 'devise/sessions#new', :as => :new_user_session_path
     post 'login' => 'devise/sessions#create', :as => :user_session_path
     get 'users/sign_out', to: 'devise/sessions#destroy'
+    get 'users/index', to: 'users#index'
+    get 'users/:id/profile_page', to: 'users#show', :as => :profile_page_path
   end
 
   devise_for :users, :controllers => { registrations: 'registrations' }
