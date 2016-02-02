f9f05de2b6c9590f931273b94bd6a5fa2cca8b2a
espa77 <ethansteiner@gmail.com>
2016-02-02 09:58:59 -0500

    security fixes

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,11 +1,10 @@
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
-      :provider               = ENV["provider"]
-      :user_name              = ENV["user_name"]
-      :aws_access_key_id      = ENV["access_key_id"]
-      :aws_secret_access_key  = ENV["secret_access_key"]
-      :region                 => 'us-east-1' # Change this for different AWS region. Default is 'us-east-1'
+      :provider => "AWS",
+      :aws_access_key_id => ENV["AWS_ACCESS_KEY"],
+      :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
+      :region => "us-east-1"
   }
-  config.fog_directory  = "petpal"
+  config.fog_directory = ENV["AWS_BUCKET"]
 end
