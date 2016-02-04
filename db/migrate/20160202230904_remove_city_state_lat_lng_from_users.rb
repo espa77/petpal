63bb1971f820e3a20790548b7a7063bedb0646d9
ycohn <cohn.yehuda@gmail.com>
2016-02-03 21:11:58 -0500

    add friends page styled

--- db/migrate/20160202230904_remove_city_state_lat_lng_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveCityStateLatLngFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :city
-    remove_column :users, :state
-    remove_column :users, :lat
-    remove_column :users, :lng
-  end
-end
