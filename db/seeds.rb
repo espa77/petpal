bd3050f95f97853a9dbd9bd85b03e98435ec8381
ycohn <cohn.yehuda@gmail.com>
2016-02-09 12:31:28 -0500

    Faker Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+30.times do
+  User.create(email: Faker::Internet.email, password: "1234", name: Faker::Name.name, gender: "Male", birthday: Faker::Date.backward(30000), species: Faker::Team.creature, avatar:Faker::Avatar.image)
+end
+
+30.times do
+  User.create(email: Faker::Internet.email, password: "1234", name: Faker::Name.name, gender: "Female", birthday: Faker::Date.backward(30000), species: Faker::Team.creature, avatar:Faker::Avatar.image)
+end
+
+
+   
