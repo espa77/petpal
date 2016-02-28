02cb6047c47d0b23182b413134a89f01695c73be
espa77 <ethansteiner@gmail.com>
2016-02-27 21:47:48 -0500

    adding jquery file that wouldn't commit last time

--- vendor/assets/javascripts/scripts.js
+++ vendor/assets/javascripts/scripts.js
@@ -1,3 +1,14 @@
+$('.card img').each(function() {
+
+  var $current =  $(this),
+      height = $current.height(),
+      max_h = $('.img-contain').height();
+  if (height > max_h) {
+    $current.css({ 'height':'100%','width': 'auto'});
+  }
+});
+
+
 
 
 /*===================================================================================*/
