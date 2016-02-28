50f1dea3a0fb2bcf19b91601be494e0c70f27523
espa77 <ethansteiner@gmail.com>
2016-02-27 22:04:32 -0500

    .js file not uploading to heroku - 3rd try

--- app/assets/javascripts/jquery-imagesizer.js
+++
@@ -1,9 +0,0 @@
-$('.card img').each(function() {
-
-  var $current =  $(this),
-      height = $current.height(),
-      max_h = $('.img-contain').height();
-  if (height > max_h) {
-    $current.css({ 'height':'100%','width': 'auto'});
-  }
-});
